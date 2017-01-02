fbae363d79f4a26c5a9c40d6759cf45e4899c6ca
Oana Munteanu <hello2oana@yahoo.com>
2017-01-02 22:23:28 +0000

    parse json and get badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,7 +5,35 @@ $(function() {
       url: 'https://www.codeschool.com/users/OanaOM.json',
       dataType: 'jsonp',
       success: function(response) {
-        // handle response
+      console.log('response', response.courses.completed);
+      addCourses(response.courses.completed);
       }
     });
+
+    function addCourses(courses){
+
+      var $badges = $('#badges');
+
+      courses.forEach(function(course){
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          href: course.url,
+          'class': 'btn btn-primary',
+          target: '_blank',
+          text: 'See course'
+        }).appendTo($course);
+
+      })
+    }
 });
