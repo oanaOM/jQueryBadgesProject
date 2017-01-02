08718865597b09e1b222e89696c9490fe81200d4
Oana Munteanu <hello2oana@yahoo.com>
2017-01-02 22:53:15 +0000

    ds

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,38 +1,20 @@
 $(function() {
-
-  // your code will go here
   $.ajax({
-      url: 'https://www.codeschool.com/users/OanaOM.json',
-      dataType: 'jsonp',
-      success: function(data) {
-      addCourses(data.courses.completed);
+    url: 'https://www.codeschool.com/users/OanaOM.json',
+    dataType: 'jsonp',
+    success: function(response){
+        addCourses(response.courses.completed);
       }
     });
 
-    function addCourses(courses){
+  function addCourses(courses) {
+    var badges = $('#badges');
+    courses.forEach(function(course) {
+      var $course = $('<div />', {'class': 'course'}).appendTo(badges);
+      $('<h3 />', {text: course.title}).appendTo($course);
+      $('<img />', {src: course.badge}).appendTo($course);
+      $('<a />', {href: course.url, target:'_blank', 'class':'btn btn-primary', text: 'See course'}).appendTo($course);
+    })
+  };
 
-      var $badges = $('#badges');
-
-      courses.forEach(function(course){
-        var $course = $('<div />', {
-          'class': 'course'
-        }).appendTo($badges);
-
-        $('<h3 />', {
-          text: course.title
-        }).appendTo($course);
-
-        $('<img />', {
-          src: course.badge
-        }).appendTo($course);
-
-        $('<a />', {
-          'class': 'btn btn-primary',
-          href: course.url,
-          target: '_blank',
-          text: 'See course'
-        }).appendTo($course);
-
-      });
-    }
 });
